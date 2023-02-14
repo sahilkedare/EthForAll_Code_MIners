b2bd5978385e7e131033d86848e26249c6e873f9
sahilkedare <sahilkedare4444@gmail.comm>
2023-02-14 23:42:34 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeScreen from "./screens/Home/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegistrationScreen from "./screens/RegistrationScreen";
+import FetchMyDocuments from "./screens/FetchMyDocuments";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+	HospitalDashboard,
+	NewRecord,
+	HospitalPastRecords,
+	HospitalProfileScreen,
+	UserRecordByAdd,
+} from "./screens/Hospital";
+
+import {
+	ResearchOrgDashboard,
+	ResearchOrgProfile,
+	NewResearch,
+	OrgResearch,
+	OrgResearchs,
+} from "./screens/ResearchOrg";
+
+import {
+	Dashboard,
+	Hospitals,
+	PastHistory,
+	ProfileScreen,
+	Research,
+	Researchs,
+} from "./screens/User";
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <HomeScreen />,
+		},
+		{
+			path: "/org/profile",
+			element: <ResearchOrgProfile />,
+		},
+		{
+			path: "/org/researchs/:researchId",
+			element: <OrgResearch />,
+		},
+		{
+			path: "createNewResearch",
+			element: <NewResearch />,
+		},
+		{
+			path: "/org/researchs",
+			element: <OrgResearchs />,
+		},
+		{
+			path: "/hospital/profile",
+			element: <HospitalProfileScreen />,
+		},
+		{ path: "/hospital/users/", element: <UserRecordByAdd /> },
+		{
+			path: "/hospital/history",
+			element: <HospitalPastRecords />,
+		},
+		{
+			path: "/user/hospitals",
+			element: <Hospitals />,
+		},
+		{
+			path: "/user/researchs",
+			element: <Researchs />,
+		},
+		{
+			path: "/researchs/:researchId",
+			element: <Research />,
+		},
+		{
+			path: "/register",
+			element: <RegistrationScreen />,
+		},
+		{
+			path: "/login",
+			element: <LoginScreen />,
+		},
+		{
+			path: "/user/profile",
+			element: <ProfileScreen />,
+		},
+		{
+			path: "/user/history",
+			element: <PastHistory />,
+		},
+		{
+			path: "/fetchmydocs",
+			element: <FetchMyDocuments />,
+		},
+		{
+			path: "createNewRecord",
+			element: <NewRecord />,
+		},
+	]);
+	return (
+		<>
+			<ToastContainer />
+			<RouterProvider router={router} />
+		</>
+	);
+};
 
 export default App;
-
